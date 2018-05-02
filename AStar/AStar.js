563b80385c2a811402fbea960fbaa5a6d5f32fb2
weidx <weidongxuan@zingfront.com>
2018-05-02 13:19:16 +0800

    修改bug

--- AStar/AStar.js
+++ AStar/AStar.js
@@ -13,8 +13,8 @@ var mapData = [
 			[0,0,1,0,0],
 			[0,0,0,0,0]
 		];
-var start = {x : 2, y : 0};
-var end = {x : 2, y : 4};
+var start = [ 2, 0];
+var end = [2, 4];
 // global fields
 var openList = [];
 var closeList = [];
@@ -22,28 +22,38 @@ var maxX = 5;
 var maxY = 5;
 var gAdd = 10;
 var hAdd = 10;
+var endId;
 class Tile{
-	constructor(pId,id,fValueValue){
+	constructor(pId,id,gValue){
 		this.pId = pId;
 		this.id = id;
+		this.gValue = gValue;
+		var hValue = computeHValue(id);
+		this.fValue = gValue+hValue?hValue:0;
+	}
+	change(pId,id,gValue,fValue){
+		this.pId = pId;
+		this.id = id;
+		this.gValue = gValue;
 		this.fValue = fValue;
 	}
 }
 function aStarFinder(start,end,gridData){	
-	var x0 = start.x;
-	var y0 = start.y;
-	var x1 = end.x;
-	var y1 = end.y;
+	var x0 = start[0];
+	var y0 = start[1];
+	var x1 = end[0];
+	var y1 = end[1];
 	if(gridData[x0][y0] == 1 || gridData[x1][y1] == 1){
 		console.log('起点或终点不可行走');
 		return;
 	}
-	var startTile = new Tile(,x0*maxY + y0,0);
-	var endId = x1*maxY+y1;
-	pushOpen(startTile,endId);
+	var startTile = new Tile(-1,x0*maxY + y0,0);
+	endId = x1*maxY+y1;
+	closeList.push(startTile);
+	pushOpen(startTile);
 }
 
-function findBestFValue(endId){
+function findBestFValue(){
 	if(openList.length<=0){
 		console.log('无路径');
 		return;
@@ -56,90 +66,115 @@ function findBestFValue(endId){
 			cost = openList[i].fValue;
 		}
 	}
-	pushOpen(openList[index],endId);
+	var node = openList[index];
+	openList.splice(index,1);
+	closeList.push(node);
+	pushOpen(node);
 }
-function pushOpen(tile,endId){
+function pushOpen(tile){
 	var id = tile.id;
 	var x = Math.floor(id / maxY);
-	var y = id % maxY;
+	var y = Math.floor(id % maxY);
 	if(x-1>=0 && mapData[x-1][y] == 0 && !myIndexOf(closeList,id-5)){
 		var aTile = myIndexOf(openList,id - 5);
 		if(!aTile)
-			openList.push(aTile = new Tile(id,id - 5,computeFValue(tile.gValue + gAdd,id-5,endId)));
+			openList.push(aTile = new Tile(id,id - 5,tile.gValue+gAdd));
 		else{
-			var fValue = computeFValue(tile.gValue - gAdd,id - 5,endId);
+			var gValue = tile.gValue + gAdd;
+			var fValue = gValue + computeHValue(id-5);
 			if(fValue < aTile.fValue){
-				aTile.fValue = fValue;
-				aTile.pId = id;
+				aTile.change(tile.id,id-5,gValue,fValue);
 			}
 		}	
 		if(aTile.id == endId){
-			console.log('终点到了',aTile);
+			console.log('over');
+			printPath(aTile,[]);
 			return;
 		}
 	}
-	if(x+1<=maxX && mapData[x+1][y] == 0 && !myIndexOf(closeList,id+5)){
+	if(x+1<maxX && mapData[x+1][y] == 0 && !myIndexOf(closeList,id+5) ){
 		var aTile = myIndexOf(openList,id+5);
 		if(!aTile)
-			openList.push(aTile = new Tile(id,id + 5,computeFValue(tile.gValue + gAdd,id+5,endId)));
+			openList.push(aTile = new Tile(id,id + 5,tile.gValue+gAdd));
 		else{
-			var fValue = computeFValue(tile.gValue - gAdd,id+5,endId);
+			var gValue = tile.gValue + gAdd;
+			var fValue = gValue + computeHValue(id+5);
 			if(fValue < aTile.fValue){
-				aTile.fValue = fValue;
-				aTile.pId = id;
+				aTile.change(tile.id,id+5,gValue,fValue);
 			}
 		}
 		if(aTile.id == endId){
-			console.log('终点到了',aTile);
+			console.log('over');
+			printPath(aTile,[]);
 			return;
 		}		
 	}
 	if(y-1>=0 && mapData[x][y-1] == 0 && !myIndexOf(closeList,id-1)){
-		var aTile = myIndexOf(openList,id+1);
+		var aTile = myIndexOf(openList,id-1);
 		if(!aTile)
-			openList.push(aTile = new Tile(id,x,id - 1,computeFValue(tile.gValue + gAdd,id-1,endId)));
+			openList.push(aTile = new Tile(id,x,id - 1,tile.gValue+gAdd));
 		else{
-			var fValue = computeFValue(tile.gValue - gAdd,id-1,endId);
+			var gValue = tile.gValue + gAdd;
+			var fValue = gValue + computeHValue(id-1);
 			if(fValue < aTile.fValue){
-				aTile.fValue = fValue;
-				aTile.pId = id;
+				aTile.change(tile.id,id-1,gValue,fValue);
 			}
 		}
 		if(aTile.id == endId){
-			console.log('终点到了',aTile);
+			console.log('over');
+			printPath(aTile,[]);
 			return;
 		}
 	}
-	if(y+1<=maxY && mapData[x][y+1] == 0 && !myIndexOf(closeList,id+1)){
+	if(y+1<maxY && mapData[x][y+1] == 0 && !myIndexOf(closeList,id+1)){
 		var aTile = myIndexOf(openList,id+1);
 		if(!aTile)
-			openList.push(aTile = new Tile(id,id + 1,computeFValue(tile.gValue + gAdd,id+1,endId)));
+			openList.push(aTile = new Tile(id,id + 1,tile.gValue+gAdd));
 		else{
-			var fValue = computeFValue(tile.gValue + gAdd,id+1,endId);
+			var gValue = tile.gValue + gAdd;
+			var fValue = gValue + computeHValue(id+1);
 			if(fValue < aTile.fValue){
-				aTile.fValue = fValue;
-				aTile.pId = id;
+				aTile.change(tile.id,id+1,gValue,fValue);
 			}
 		}
 		if(aTile.id == endId){
-			console.log('终点到了',aTile);
+			console.log('over');
+			printPath(aTile,[]);
 			return;
 		}
 	}
-	findBestFValue(endId);
+	findBestFValue();
 }
 function myIndexOf(list,id){
 	for(var i = 0; i < list.length; i++){
-		if(list[i].id == id) return true;
+		if(list[i].id == id) return list[i];
 	}
-	return false;
+	return null;
 }
-function computeFValue(gValue,id,endId){
+function myIndexOf2(id){
+	for(var i = 0; i < openList.length; i++){
+		if(openList[i].id == id) return openList[i];
+	}
+	for(var i = 0; i < closeList.length; i++){
+		if(closeList[i].id == id) return closeList[i];
+	}
+}
+function computeHValue(id){
 	var x0 = Math.floor(id / maxY);
 	var y0 = id % maxY;
 	var x1 = Math.floor(endId / maxY);
 	var y1 = endId % maxY;
 	var hValue = (Math.abs(x0-x1)+Math.abs(y0-y1))*hAdd;
-	return gValue + hValue;
+	return hValue;
+}
+function printPath(tile,arr){
+	var id = tile.id;
+	arr.push([Math.floor(id/maxX),id%maxX]);
+	if(tile.pId == -1){
+		console.log(arr.reverse());
+		return;	
+	} 
+	var aTile = myIndexOf2(tile.pId);
+	printPath(aTile,arr);
 }
 aStarFinder(start,end,mapData);
